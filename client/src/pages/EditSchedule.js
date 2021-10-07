875a2aa55cdb2ada420bc5d0b295dab386413233
Pavel Nedosika <nedosika@gmail.com>
2021-10-07 20:13:15 +0300

    add calendar

--- client/src/pages/EditSchedule.js
+++ client/src/pages/EditSchedule.js
@@ -2,39 +2,31 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import {useHistory, useParams} from "react-router-dom";
 
-import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Fab from "@mui/material/Fab";
-import SaveIcon from '@mui/icons-material/Save';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import Stack from "@mui/material/Stack";
 import Slide from '@mui/material/Slide';
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogActions from "@mui/material/DialogActions";
-import Dialog from "@mui/material/Dialog";
-
-import BackButton from "../components/BackButton";
-import useActions from "../hooks/useActions";
-import {RouteNames} from "../utils/consts";
-import Layout from "../components/Layout";
-import Masonry from "@mui/lab/Masonry";
-import MasonryItem from "@mui/lab/MasonryItem";
-
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import TableRow from '@mui/material/TableRow';
+import SaveIcon from '@mui/icons-material/Save';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import IconButton from '@mui/material/IconButton';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import TableContainer from '@mui/material/TableContainer';
+import DialogContentText from "@mui/material/DialogContentText";
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+
+import Layout from "../components/Layout";
+import {RouteNames} from "../utils/consts";
+import useActions from "../hooks/useActions";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
@@ -43,9 +35,9 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 const EditSchedule = () => {
     const {id} = useParams();
     const history = useHistory();
-    const {updateSchedule, deleteSchedule} = useActions();
+    const {deleteSchedule} = useActions();
+    const {users} = useSelector(state => state.users);
     const schedule = useSelector(state => state.schedules.schedules.find((schedule) => schedule.id === id));
-    const [open, setOpen] = React.useState(false);
 
     const today = new Date();
     const month = today.getMonth();
@@ -57,26 +49,9 @@ const EditSchedule = () => {
     for (let i = 1; i <= numberDaysOfMonth; i++)
         days.push(i);
 
-    const [state, setState] = React.useState({
-        ...schedule
-    });
     const [isDeleteDialogOpen, setIsDeleteDialogOpen] = React.useState(false);
 
-    const handleChange = (event) => {
-        setState({
-            ...state,
-            [event.target.name]: event.target.value
-        });
-    };
-
-    const handleSubmit = () => {
-        updateSchedule({...state})
-            .then(() => {
-                history.push(RouteNames.SCHEDULES)
-            });
-    };
-
-    const handleDeleteUser = () => {
+    const handleDeleteSchedule = () => {
         deleteSchedule(id)
             .then(() => history.push(RouteNames.SCHEDULES));
     };
@@ -84,7 +59,6 @@ const EditSchedule = () => {
     const handleCloseDeleteDialog = () => {
         setIsDeleteDialogOpen(false);
     };
-
 
     return (
         <Layout title="Edit schedule" backButton>
@@ -96,80 +70,38 @@ const EditSchedule = () => {
                     justifyContent: 'center'
                 }}
             >
-
-                {/*<Masonry columns={7} spacing={2} sx={{maxWidth: '50%'}}>*/}
-                {/*    {days.map((item, index) => (*/}
-                {/*        <MasonryItem key={index}>*/}
-                {/*            <Button variant="outlined">{item}</Button>*/}
-                {/*        </MasonryItem>*/}
-                {/*    ))}*/}
-                {/*</Masonry>*/}
-
                 {
                     <TableContainer component={Paper}>
                         <Table aria-label="collapsible table">
                             <TableHead>
                                 <TableRow>
-                                    <TableCell />
                                     <TableCell>User</TableCell>
                                     {
-                                        days.map((item) => <TableCell align="right">{item}</TableCell>)
+                                        days.map((item) =>
+                                            <TableCell align="center">{item}</TableCell>
+                                        )
                                     }
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                <React.Fragment>
-                                    <TableRow sx={{'& > *': {borderBottom: 'unset'}}}>
-                                        <TableCell>
-                                            <IconButton
-                                                aria-label="expand row"
-                                                size="small"
-                                                onClick={() => setOpen(!open)}
-                                            >
-                                                {open ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
-                                            </IconButton>
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            User
-                                        </TableCell>
-                                        {
-                                            days.map((item) => <TableCell align="right">{item}</TableCell>)
-                                        }
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell style={{paddingBottom: 0, paddingTop: 0}} colSpan={6}>
-                                            <Collapse in={open} timeout="auto" unmountOnExit>
-                                                <Box sx={{margin: 1}}>
-                                                    <Typography variant="h6" gutterBottom component="div">
-                                                        History
-                                                    </Typography>
-                                                    <Table size="small" aria-label="purchases">
-                                                        <TableHead>
-                                                            <TableRow>
-                                                                <TableCell>Date</TableCell>
-                                                                <TableCell>Customer</TableCell>
-                                                                <TableCell align="right">Amount</TableCell>
-                                                                <TableCell align="right">Total price ($)</TableCell>
-                                                            </TableRow>
-                                                        </TableHead>
-                                                        <TableBody>
-                                                            <TableRow>
-                                                                <TableCell component="th" scope="row">
-                                                                    ffff
-                                                                </TableCell>
-                                                                <TableCell>111</TableCell>
-                                                                <TableCell align="right">222</TableCell>
-                                                                <TableCell align="right">
-                                                                    qqq
-                                                                </TableCell>
-                                                            </TableRow>
-                                                        </TableBody>
-                                                    </Table>
-                                                </Box>
-                                            </Collapse>
-                                        </TableCell>
-                                    </TableRow>
-                                </React.Fragment>
+                                {
+                                    users.map((user) =>
+                                        <TableRow sx={{'& > *': {borderBottom: 'unset'}}}>
+                                            <TableCell component="th" scope="row">
+                                                {user.username}
+                                            </TableCell>
+                                            {
+                                                days.map((item) =>
+                                                    <TableCell align="center" key={item}>
+                                                        <IconButton size="small">
+                                                            <AlarmIcon fontSize="inherit"/>
+                                                        </IconButton>
+                                                    </TableCell>
+                                                )
+                                            }
+                                        </TableRow>
+                                    )
+                                }
                             </TableBody>
                         </Table>
                     </TableContainer>
@@ -193,13 +125,6 @@ const EditSchedule = () => {
                 >
                     <DeleteOutlineIcon/>
                 </Fab>
-                <Fab
-                    size="medium"
-                    color="primary"
-                    onClick={handleSubmit}
-                >
-                    <SaveIcon/>
-                </Fab>
             </Stack>
             <Dialog
                 open={isDeleteDialogOpen}
@@ -215,7 +140,7 @@ const EditSchedule = () => {
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleCloseDeleteDialog}>Disagree</Button>
-                    <Button onClick={handleDeleteUser}>Agree</Button>
+                    <Button onClick={handleDeleteSchedule}>Agree</Button>
                 </DialogActions>
             </Dialog>
         </Layout>
